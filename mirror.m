082750067f4b37c51c8a28ff5c7d6b1ac9b8f16d
LiliMeng <lilimeng1103@gmail.com>
2015-12-29 21:06:53 -0800

    Create mirror.m

---
+++ mirror.m
@@ -0,0 +1,13 @@
+
+%镜像
+hold on
+   plot(-coordinates(:,1), -coordinates(:,2));
+mirror only in x axis
+
+   hold on
+       plot(coordinates(:,1), -coordinates(:,2));
+mirror only in y axis
+
+   hold on
+       plot(-coordinates(:,1), coordinates(:,2));
+*
